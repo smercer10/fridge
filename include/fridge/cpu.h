59600ee4517f0af05fa21224a7c6a3e8393a69ca
Sam Mercer <130914459+smercer10@users.noreply.github.com>
2025-04-12 14:14:25 +0100

    Implement CPU memory bus

--- include/fridge/cpu.h
+++ include/fridge/cpu.h
@@ -1,8 +1,7 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
-
-namespace CPU {
 
 struct FlagsRegister {
     bool z : 1;
@@ -19,7 +18,6 @@ struct Registers {
         };
         uint16_t af;
     };
-
     union {
         struct {
             uint8_t bcLo;
@@ -27,7 +25,6 @@ struct Registers {
         };
         uint16_t bc;
     };
-
     union {
         struct {
             uint8_t deLo;
@@ -35,7 +32,6 @@ struct Registers {
         };
         uint16_t de;
     };
-
     union {
         struct {
             uint8_t hlLo;
@@ -43,11 +39,31 @@ struct Registers {
         };
         uint16_t hl;
     };
-
     uint16_t sp;
     uint16_t pc;
-
     FlagsRegister flags;
 };
 
-} // namespace CPU
+class MemoryBus {
+public:
+    [[nodiscard]] uint8_t read(uint16_t addr) const noexcept
+    {
+        return mem[addr];
+    }
+
+    void write(uint16_t addr, uint8_t val) noexcept
+    {
+        mem[addr] = val;
+    }
+
+private:
+    static constexpr std::size_t memSize { 0x10000 }; // 64KB memory space.
+    std::array<uint8_t, memSize> mem {};
+};
+
+class CPU {
+public:
+private:
+    Registers reg {};
+    MemoryBus mem;
+};
